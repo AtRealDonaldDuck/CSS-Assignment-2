788e792c10dea493f79e058087bb31b900f649ae
AtRealDonaldDuck <69385515+AtRealDonaldDuck@users.noreply.github.com>
2023-01-09 01:36:33 +0100

    jan 9th update

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 function ValidateAndCalculate(){    
     var userInputs = document.querySelectorAll('#CustomizationOptions > div > *');
-    var response = ""//use this in the alert box to show every incorrect element 
+    var error = false;
 
     Array.from(userInputs).forEach(child => {
-        if (child.tagName == 'SELECT' && child.options[child.selectedIndex].value == "placeholder") {//child[0] refers to the selected option in the select tag
-            child.style.color = 'red';
-        }else if (child.tagName == 'INPUT' && child.type == 'text' && child.value=="") {
-            child.style.background = 'red';//setting text colour or border color changes nothing so now you get red everything... i hope youre happy
+        if (child.tagName == 'SELECT') {//child[0] refers to the selected option in the select tag, the visibility condition accounts for sub controls.
+            if (child.options[child.selectedIndex].value == "placeholder" || child.parentElement.style.display != '') {
+                child.style.color = 'red';
+                error = true;
+            } else{
+                child.style.color = 'black';
+                error = false;
+            }
+        }else if (child.tagName == 'INPUT' && child.type == 'text') {
+            if (child.value=='') {
+                child.style.background = 'red';//setting text colour or border color changes nothing so now you get red everything... i hope youre happy
+                error = true;
+            }else{
+                child.style.background = '';
+                error = false;
+            }
         }
     });
+    if (error) {
+        ShowErrorMessage();
+    }else{
+        /*
+        -----------------------------------------------
+        ---=========================================---
+        -==calculate final price and display a quote==-
+        ---=========================================---
+        -----------------------------------------------
+        */
+    }
+}
+function ShowErrorMessage(){
+    alert("Please check your inputs and try again.")
+}
+function SetVisibility(element, visible){
+    element.style.display = visible ? 'block' : '';
+}
+
+function ValidateForm(form){
+    var error = false;
+    var inputs = form.querySelectorAll('.form-control');
+    inputs.forEach(input => {
+        if(input.value == ''){
+            input.style.borderColor = 'red';
+            error = true;
+        }else{
+            input.style.borderColor = 'green';
+            error = false;
+        }
+    });
+    if (error) {
+        alert("ALL inputs are mandatory,please ammend your entries and try again");
+    }
 }
