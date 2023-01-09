fa8812a3dfb691083bb1904279f11eff6734d6e0
AtRealDonaldDuck <69385515+AtRealDonaldDuck@users.noreply.github.com>
2023-01-09 23:04:56 +0100

    almost done

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ function ValidateAndCalculate(){
     var error = false;
 
     Array.from(userInputs).forEach(child => {
+        if (child.options[child.selectedIndex].value == "placeholder" || child.parentElement.style.display != '') {
         if (child.tagName == 'SELECT') {//child[0] refers to the selected option in the select tag, the visibility condition accounts for sub controls.
-            if (child.options[child.selectedIndex].value == "placeholder" || child.parentElement.style.display != '') {
                 child.style.color = 'red';
                 error = true;
             } else{
                 child.style.color = 'black';
-                error = false;
             }
         }else if (child.tagName == 'INPUT' && child.type == 'text') {
             if (child.value=='') {
@@ -17,7 +16,6 @@ function ValidateAndCalculate(){
                 error = true;
             }else{
                 child.style.background = '';
-                error = false;
             }
         }
     });
@@ -47,12 +45,19 @@ function ValidateForm(form){
         if(input.value == ''){
             input.style.borderColor = 'red';
             error = true;
+        }else if (input.id == 'phoneNumber' && input.value.length != 8) {
+            input.style.borderColor = 'red';
+            error = true;
         }else{
             input.style.borderColor = 'green';
-            error = false;
         }
     });
     if (error) {
         alert("ALL inputs are mandatory,please ammend your entries and try again");
     }
+}
+
+function restrictToNumbers(element){
+    var text = element.value;
+    element.value = text.replace(/\D/g,'');
 }
